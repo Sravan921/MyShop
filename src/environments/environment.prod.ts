8a87c9f3691ad9c7fb31c89267b5383278d21cff
Macbook Pro <sravan@Sravans-MacBook-Pro.local>
2018-02-05 12:01:42 -0600

    FireBase Integrated
    Firebase Added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDGtzE_djutJFIodf9_fiq_bQzW5GIYZ1c",
+    authDomain: "myshop-79309.firebaseapp.com",
+    databaseURL: "https://myshop-79309.firebaseio.com",
+    projectId: "myshop-79309",
+    storageBucket: "",
+    messagingSenderId: "1035162512492"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDGtzE_djutJFIodf9_fiq_bQzW5GIYZ1c",
+    authDomain: "myshop-79309.firebaseapp.com",
+    databaseURL: "https://myshop-79309.firebaseio.com",
+    projectId: "myshop-79309",
+    storageBucket: "",
+    messagingSenderId: "1035162512492"
+  }
 };
